21f6521726b0df3a766b3422a24e25ce6e206013
rahultiwari.201307@gmail.com <46418123+Rahul-71@users.noreply.github.com>
2023-05-24 10:38:30 +0530

    cartContext using useState api

--- src/cart-context/CartContext.js
+++ src/cart-context/CartContext.js
@@ -1,6 +1,6 @@
-import React, { useContext, useEffect, useReducer, useState } from "react";
-import { AuthContext } from "./AuthContext";
+import React, { useContext, useEffect, useState } from "react";
 import { fetchCartItems, updateCart } from "../Utils/UtilityFunctions";
+import { AuthContext } from "./AuthContext";
 
 export const CartContext = React.createContext({
   items: [],
@@ -17,132 +17,85 @@ const defCartState = {
   isCartShown: false,
 };
 
-const ACTIONS = {
-  ADD_ITEM: "addItem",
-  REMOVE_ITEM: "removeItem",
-  TOGGLE_CART: "toggleCart",
-  SET_INITIAL_STATE: "setInitialState",
-};
-
-const cartReducer = (state, action) => {
-  const { items } = state;
-
-  switch (action.type) {
-    case ACTIONS.ADD_ITEM:
-      const existingItem = items.find((item) => item.id === action.item.id);
-
-      if (existingItem) {
-        window.alert("Already added into the cart");
-        return state;
-      }
-
-      const updatedItems = [...items, action.item];
-      const totalPrice =
-        Number.parseInt(action.item.price) + Number.parseInt(state.totalPrice);
-
-      updateCart({ items: updatedItems, totalPrice }, action.emailId)
-        .then((res) => {})
-        .catch((err) => {});
-
-      return { ...state, items: updatedItems, totalPrice };
-
-    case ACTIONS.REMOVE_ITEM:
-      const itemToRemoveIndx = items.findIndex(
-        (item) => +item.id === +action.id
-      );
-
-      const updatedState = {
-        ...state,
-        items: [
-          ...state.items.slice(0, itemToRemoveIndx),
-          ...state.items.slice(itemToRemoveIndx + 1),
-        ],
-        totalPrice:
-          Number.parseInt(state.totalPrice) -
-          Number.parseInt(state.items[itemToRemoveIndx].price),
-      };
-
-      updateCart(updatedState, action.emailId)
-        .then((res) => {})
-        .catch((err) => {});
-
-      return updatedState;
-
-    case ACTIONS.TOGGLE_CART:
-      return { ...state, isCartShown: !state.isCartShown };
-
-    case ACTIONS.SET_INITIAL_STATE:
-      return { ...state, ...action.payload };
-
-    default:
-      return state;
-  }
-};
-
 const CartContextProvider = ({ children }) => {
   const authCtx = useContext(AuthContext);
+  const { emailId, isLoggedIn } = authCtx;
 
-  const [cartState, cartActionDispatch] = useReducer(cartReducer, defCartState);
+  const [cartState, setCartState] = useState(defCartState);
 
   useEffect(() => {
     const fetchInitialState = async () => {
       try {
-        // Use destructuring to get the emailId from authCtx
-        const { emailId } = authCtx;
-        // Use await and then to get the response and dispatch the action
-        await fetchCartItems(emailId).then((response) =>
-          cartActionDispatch({
-            type: ACTIONS.SET_INITIAL_STATE,
-            payload: response,
-          })
-        );
+        const response = await fetchCartItems(emailId);
+        setCartState((prevState) => ({ ...prevState, ...response }));
       } catch (error) {
         console.log(
-          `error occured while fetching cart items : ${error.message}`
+          `Error occurred while fetching cart items: ${error.message}`
         );
       }
     };
 
-    // Use a ternary operator to simplify the conditional logic
-    authCtx.isLoggedIn
-      ? fetchInitialState()
-      : cartActionDispatch({
-          type: ACTIONS.SET_INITIAL_STATE,
-          payload: defCartState,
-        });
-  }, [authCtx.isLoggedIn]);
+    isLoggedIn ? fetchInitialState() : setCartState(defCartState);
+  }, [isLoggedIn, emailId]);
 
-  const addItemDispatch = (item) => {
-    cartActionDispatch({
-      type: ACTIONS.ADD_ITEM,
-      item: item,
-      emailId: authCtx.emailId,
+  const addItem = (item) => {
+    setCartState((prevState) => {
+      const existingItem = prevState.items.find(
+        (prevItem) => prevItem.id === item.id
+      );
+
+      if (existingItem) {
+        window.alert("Already added into the cart");
+        return prevState;
+      }
+
+      const updatedItems = [...prevState.items, item];
+      const totalPrice =
+        Number.parseInt(item.price) + Number.parseInt(prevState.totalPrice);
+
+      updateCart({ items: updatedItems, totalPrice }, emailId)
+        .then(() => {})
+        .catch((err) => console.log(`Error occurred: ${err}`));
+
+      return { ...prevState, items: updatedItems, totalPrice };
     });
   };
 
-  const removeItemDispatch = (id) => {
-    cartActionDispatch({
-      type: ACTIONS.REMOVE_ITEM,
-      id: id,
-      emailId: authCtx.emailId,
+  const removeItem = (id) => {
+    setCartState((prevState) => {
+      const itemToRemoveIndex = prevState.items.findIndex(
+        (item) => +item.id === +id
+      );
+      const updatedItems = [
+        ...prevState.items.slice(0, itemToRemoveIndex),
+        ...prevState.items.slice(itemToRemoveIndex + 1),
+      ];
+      const totalPrice =
+        Number.parseInt(prevState.totalPrice) -
+        Number.parseInt(prevState.items[itemToRemoveIndex].price);
+
+      updateCart({ items: updatedItems, totalPrice }, emailId)
+        .then(() => {})
+        .catch((err) => console.log(`Error occurred: ${err}`));
+
+      return { ...prevState, items: updatedItems, totalPrice };
     });
   };
 
-  const toggleCart = () => {
-    cartActionDispatch({ type: ACTIONS.TOGGLE_CART });
+  const toggleCartShown = () => {
+    setCartState((prevState) => ({
+      ...prevState,
+      isCartShown: !prevState.isCartShown,
+    }));
   };
 
   const cartContextValue = {
     items: cartState.items,
-    addItem: (item) => {
-      addItemDispatch(item);
-    },
-    removeItem: (id) => {
-      removeItemDispatch(id);
-    },
+    addItem,
+    removeItem,
     totalPrice: cartState.totalPrice,
     isCartShown: cartState.isCartShown,
-    toggleCartShown: toggleCart,
+    toggleCartShown,
   };
 
   return (
@@ -153,3 +106,161 @@ const CartContextProvider = ({ children }) => {
 };
 
 export default CartContextProvider;
+
+/////////////////////// using useReducer ///////////////////////
+
+// import React, { useContext, useEffect, useReducer } from "react";
+// import { fetchCartItems, updateCart } from "../Utils/UtilityFunctions";
+// import { AuthContext } from "./AuthContext";
+
+// export const CartContext = React.createContext({
+//   items: [],
+//   addItem: (item) => {},
+//   removeItem: (id) => {},
+//   totalPrice: 0,
+//   isCartShown: false,
+//   toggleCartShown: () => {},
+// });
+
+// const defCartState = {
+//   items: [],
+//   totalPrice: 0,
+//   isCartShown: false,
+// };
+
+// const ACTIONS = {
+//   ADD_ITEM: "addItem",
+//   REMOVE_ITEM: "removeItem",
+//   TOGGLE_CART: "toggleCart",
+//   SET_INITIAL_STATE: "setInitialState",
+// };
+
+// const cartReducer = (state, action) => {
+//   const { items } = state;
+
+//   switch (action.type) {
+//     case ACTIONS.ADD_ITEM:
+//       const existingItem = items.find((item) => item.id === action.item.id);
+
+//       if (existingItem) {
+//         window.alert("Already added into the cart");
+//         return state;
+//       }
+
+//       const updatedItems = [...items, action.item];
+//       const totalPrice =
+//         Number.parseInt(action.item.price) + Number.parseInt(state.totalPrice);
+
+//       updateCart({ items: updatedItems, totalPrice }, action.emailId)
+//         .then((res) => {})
+//         .catch((err) => {});
+
+//       return { ...state, items: updatedItems, totalPrice };
+
+//     case ACTIONS.REMOVE_ITEM:
+//       const itemToRemoveIndx = items.findIndex(
+//         (item) => +item.id === +action.id
+//       );
+
+//       const updatedState = {
+//         ...state,
+//         items: [
+//           ...state.items.slice(0, itemToRemoveIndx),
+//           ...state.items.slice(itemToRemoveIndx + 1),
+//         ],
+//         totalPrice:
+//           Number.parseInt(state.totalPrice) -
+//           Number.parseInt(state.items[itemToRemoveIndx].price),
+//       };
+
+//       updateCart(updatedState, action.emailId)
+//         .then((res) => {})
+//         .catch((err) => {});
+
+//       return updatedState;
+
+//     case ACTIONS.TOGGLE_CART:
+//       return { ...state, isCartShown: !state.isCartShown };
+
+//     case ACTIONS.SET_INITIAL_STATE:
+//       return { ...state, ...action.payload };
+
+//     default:
+//       return state;
+//   }
+// };
+
+// const CartContextProvider = ({ children }) => {
+//   const authCtx = useContext(AuthContext);
+
+//   const [cartState, cartActionDispatch] = useReducer(cartReducer, defCartState);
+
+//   useEffect(() => {
+//     const fetchInitialState = async () => {
+//       try {
+//         // Use destructuring to get the emailId from authCtx
+//         const { emailId } = authCtx;
+//         // Use await and then to get the response and dispatch the action
+//         await fetchCartItems(emailId).then((response) =>
+//           cartActionDispatch({
+//             type: ACTIONS.SET_INITIAL_STATE,
+//             payload: response,
+//           })
+//         );
+//       } catch (error) {
+//         console.log(
+//           `error occured while fetching cart items : ${error.message}`
+//         );
+//       }
+//     };
+
+//     // Use a ternary operator to simplify the conditional logic
+//     authCtx.isLoggedIn
+//       ? fetchInitialState()
+//       : cartActionDispatch({
+//           type: ACTIONS.SET_INITIAL_STATE,
+//           payload: defCartState,
+//         });
+//   }, [authCtx.isLoggedIn]);
+
+//   const addItemDispatch = (item) => {
+//     cartActionDispatch({
+//       type: ACTIONS.ADD_ITEM,
+//       item: item,
+//       emailId: authCtx.emailId,
+//     });
+//   };
+
+//   const removeItemDispatch = (id) => {
+//     cartActionDispatch({
+//       type: ACTIONS.REMOVE_ITEM,
+//       id: id,
+//       emailId: authCtx.emailId,
+//     });
+//   };
+
+//   const toggleCart = () => {
+//     cartActionDispatch({ type: ACTIONS.TOGGLE_CART });
+//   };
+
+//   const cartContextValue = {
+//     items: cartState.items,
+//     addItem: (item) => {
+//       addItemDispatch(item);
+//     },
+//     removeItem: (id) => {
+//       removeItemDispatch(id);
+//     },
+//     totalPrice: cartState.totalPrice,
+//     isCartShown: cartState.isCartShown,
+//     toggleCartShown: toggleCart,
+//   };
+
+//   return (
+//     <CartContext.Provider value={cartContextValue}>
+//       {children}
+//     </CartContext.Provider>
+//   );
+// };
+
+// export default CartContextProvider;
